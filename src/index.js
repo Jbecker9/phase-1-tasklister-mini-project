34f9a451c5fc1bafcf77f8fda5b7e40da4f97602
Jared <jbeckersalax9@aol.com>
2021-12-12 16:49:10 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('#main-content').addEventListener('submit', (e) => {
+    e.preventDefault();
+    submitList(e.target.maincontent.value)
+  })
 });
+
+function submitList(maincontent){
+  let li = document.body.div.createElement('li');
+  p.textContent = maincontent;
+  document.body.div.querySelector('#tasks').appendChild(li)
+}
